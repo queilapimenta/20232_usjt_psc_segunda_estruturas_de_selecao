605ee231deeb5972cabaa7a3be4b269f1d06edf7
Queila Caroline <queilapimenta10@gmail.com>
2023-09-11 21:08:22 -0300

    ap03(selecao): fim do exemplo if

--- exemploif.java
+++ exemploif.java
@@ -3,11 +3,14 @@ import javax.swing.JOptionPane;
 public class exemploif {
   static public void main (String [] args){
     //declaração de variáveis 
-
     //entrada
-
+    double nota = 
+      Double.parseDouble(JOptionPane.showInputDialog
+      ("Digite a nota"));
     //processamento  
-
     //saída
+    if (nota >= 70){
+       JOptionPane.showInputDialog(null, "Aprovado");
+    }
   }
 }
